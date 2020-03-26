92214bfd89bf8523eef9d36505463bbb3746a212
wb214476273 <wb214476273@hotmail.com>
2020-03-26 21:02:53 +0800

    计算器2

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,107 +1,142 @@
-import React, {Component} from "react";
-import {AppRegistry, StyleSheet, Text, View, Alert, Button,} from "react-native";
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    PixelRatio,
+    StatusBar
+} from 'react-native';
 
-export default class FlexBoxDemoActivity extends Component {
-        render() {
+//获取屏幕长宽
+var ScreenWidth = Dimensions.get('window').width;
+var ScreenHeight = Dimensions.get('window').height;
+
+export default class OZCalculator extends Component {
+    render() {
         return (
-                    <View style={FlexBoxStyle.rootview}>
-
-                        {/*显示*/}
-                        <Text style={FlexBoxStyle.display}>显示区域</Text>
-
-                        <View style={FlexBoxStyle.numKey}>
-                            {/*按键*/}
-                            <Text style={FlexBoxStyle.numItem}>9</Text>
-                            <Text style={FlexBoxStyle.numItem}>8</Text>
-                            <Text style={FlexBoxStyle.numItem}>7</Text>
-                            <Text style={FlexBoxStyle.numItem}>6</Text>
-                            <Text style={FlexBoxStyle.numItem}>5</Text>
-                            <Text style={FlexBoxStyle.numItem}>4</Text>
-                            <Text style={FlexBoxStyle.numItem}>3</Text>
-                            <Text style={FlexBoxStyle.numItem}>2</Text>
-                            <Text style={FlexBoxStyle.numItem}>1</Text>
-                            <Text style={FlexBoxStyle.numItem}>=</Text>
-                            <Text style={FlexBoxStyle.numItem}>.</Text>
-                            <Text style={FlexBoxStyle.numItem}>0</Text>
-                        </View>
-
-                        {/*右侧逻辑运算符区域*/}
-                        <View style={FlexBoxStyle.option}>
-                            <Text style={FlexBoxStyle.numItem}>+</Text>
-                            <Text style={FlexBoxStyle.numItem}>CE</Text>
-                            <Text style={FlexBoxStyle.numItem}>-</Text>
-                            <Text style={FlexBoxStyle.numItem}>C</Text>
-                            <Text style={FlexBoxStyle.numItem}>*</Text>
-                            <Text style={FlexBoxStyle.numItem}>sin</Text>
-                            <Text style={FlexBoxStyle.numItem}>%</Text>
-                            <Text style={FlexBoxStyle.numItem}>cos</Text>
-
-                        </View>
-
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content"/>
+                <View style={styles.lineContainer}>
+                    <View style={[styles.item,{flexGrow: 2}]}>
+                        <Text style={[styles.itemNumber, {paddingRight: ScreenWidth/4}]}>0</Text>
                     </View>
+                    <View style={[styles.item]}>
+                        <Text style={styles.itemNumber}>.</Text>
+                    </View>
+                    <View style={[styles.item,{backgroundColor: '#fa8c13'}]}>
+                        <Text style={styles.itemSymbol}>=</Text>
+                    </View>
+                </View>
+                <View style={styles.lineContainer}>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>1</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>2</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>3</Text>
+                    </View>
+                    <View style={[styles.item,styles.symbolItemColor]}>
+                        <Text style={styles.itemSymbol}>+</Text>
+                    </View>
+                </View>
+                <View style={styles.lineContainer}>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>4</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>5</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>6</Text>
+                    </View>
+                    <View style={[styles.item,styles.symbolItemColor]}>
+                        <Text style={styles.itemSymbol}>-</Text>
+                    </View>
+                </View>
+                <View style={styles.lineContainer}>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>7</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>8</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.itemNumber}>9</Text>
+                    </View>
+                    <View style={[styles.item,styles.symbolItemColor]}>
+                        <Text style={styles.itemSymbol}>×</Text>
+                    </View>
+                </View>
+                <View style={styles.lineContainer}>
+                    <View style={[styles.item,styles.topItemColor]}>
+                        <Text style={styles.itemNumber}>AC</Text>
+                    </View>
+                    <View style={[styles.item,styles.topItemColor]}>
+                        <Text style={styles.itemNumber}>±</Text>
+                    </View>
+                    <View style={[styles.item,styles.topItemColor]}>
+                        <Text style={styles.itemNumber}>%</Text>
+                    </View>
+                    <View style={[styles.item,styles.symbolItemColor]}>
+                        <Text style={styles.itemSymbol}>÷</Text>
+                    </View>
+                </View>
+                <View style={styles.result}>
+                    <Text style={styles.resultText}>
 
-                )
-     }
+                    </Text>
+                </View>
+            </View>
+        );
+    }
 }
 
-const FlexBoxStyle = StyleSheet.create({
-
-rootview: {
-    flexDirection: "row",
-    flexWrap: "wrap"
-},
-
-display: {
-    //相当于TtextView的gravity=center效果
-    textAlign: "right",
-    textAlignVertical: "center",
-
-    //相当于android里面的weight
-    // flexGrow: 1,
-
-    padding: 10,
-    marginRight: 10,
-    backgroundColor: "grey",
-    width: "100%",
-    // boxSizing: content-box|border-box|inherit;
-    height: 50
-},
-
-//数字按键
-numKey:{
-    flex:3,
-    flexWrap:"wrap",
-    padding:10,
-    backgroundColor:"green",
-    flexDirection:"row-reverse",
-    justifyContent: 'space-around'
-},
-
-//右侧逻辑运算符
-option:{
-    paddingTop:10,
-    paddingLeft:10,
-    flex:2,
-    flexWrap:"wrap",
-    backgroundColor:"blue",
-    flexDirection:"row",
-    justifyContent: 'space-around'
-},
-
-numItem:{
-    backgroundColor: "#f0f",
-    marginTop: 10,
-
-    //相当于TtextView的gravity=center效果
-    textAlign: "center",
-    textAlignVertical: "center",
-
-    //和androidMarginRight一样
-    marginRight: 20,
-
-    alignContent: "center",
-    width: 40,
-    height: 40,
-
-}
-})
+const styles = StyleSheet.create({
+    container: {
+        height: ScreenHeight,
+        flexDirection: 'column-reverse',
+    },
+    lineContainer: {
+        height: ScreenWidth / 4,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    item: {
+        backgroundColor: '#d5d6d8',
+        borderColor: '#000',
+        //这里通过PixelRatiol来获取最小线宽
+        borderWidth: 1,
+        flex: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    topItemColor:{
+        backgroundColor: '#c9cacc',
+    },
+    symbolItemColor:{
+        backgroundColor: '#fa8c13',
+    },
+    itemNumber: {
+        fontSize: 30,
+    },
+    itemSymbol: {
+        color:'#FFF',
+        fontSize: 30,
+    },
+    result: {
+        backgroundColor: '#000',
+        flexDirection: 'column-reverse',
+        alignItems: 'flex-end',
+        flex: 1,
+    },
+    resultText: {
+        color: '#FFF',
+        marginBottom: 10,
+        fontSize: 40,
+        fontWeight: 'bold',
+    }
+});
